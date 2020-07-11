5491ad54e26111204f68531ef5820bf8d413ef1b
barto14753 <wlodarski_bartosz@o2.pl>
2020-07-11 23:30:19 +0200

    .

---
+++ graph.py
@@ -0,0 +1,10 @@
+import pygame
+import random
+from colors import Color
+
+colors = Color()
+
+
+class Top:
+	def __init__(self, screen, )
+
---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+from colors import Color
+
+colors = Color()
+
+WIDTH = 800
+HEIGHT = 600
+
+BACKGROUND_COLOR = colors.white
+
+
+
