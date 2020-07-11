df04c96b6d1ce1c2aa6ac75eab2168c8ef1284ce
barto14753 <wlodarski_bartosz@o2.pl>
2020-07-11 23:33:42 +0200

    .

---
+++ colors.py
@@ -0,0 +1,26 @@
+class Color:
+	def __init__(self):
+		self.black = (0,0,0)
+		self.white = (255,255,255)
+		self.red = (255,0,0)
+		self.green = (0,255,0)
+		self.blue = (0,0,255)
+
+		self.dark_purple = (46,7,100)
+		self.purple = (82,16,100)
+		self.light_purple = (106,41,100)
+
+		self.dark_pink = (115,3.100)
+		self.pink = (150,17,100)
+		self.light_pink = (211,38,100)
+
+		self.light_green = (134,248,100)
+		self.dark_green = (4,145,100)
+
+		self.light_red = (247,50,0)
+		self.dark_red = (140,2,0)
+
+		self.light_blue = (3,254,200)
+		self.dark_blue = (1,0.200)
+
+		self.orange = (248,118,0)
